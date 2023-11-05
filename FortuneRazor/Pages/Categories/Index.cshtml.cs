095bc261e2cf906db6a134e8c2a25892f899d606
DigitechPC <DigitechPC@DESKTOP-P9PA386>
2023-11-05 20:04:30 +0530

    Added First Razor Page

---
+++ FortuneRazor/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FortuneRazor.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
