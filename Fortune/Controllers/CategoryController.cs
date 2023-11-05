12b2915cb994150cd43ad4b04590c22b690b6155
DigitechPC <DigitechPC@DESKTOP-P9PA386>
2023-11-05 19:28:16 +0530

    Completed the full crud Functionality

--- Fortune/Controllers/CategoryController.cs
+++ Fortune/Controllers/CategoryController.cs
@@ -6,7 +6,7 @@ namespace Fortune.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDBContext _dbContext;   
+        private readonly ApplicationDBContext _dbContext;
         public CategoryController(ApplicationDBContext dbContext)
         {
             _dbContext = dbContext;
@@ -24,8 +24,110 @@ namespace Fortune.Controllers
         [HttpPost]
         public IActionResult Create(Category Category)
         {
-            _dbContext.Category.Add(Category);
+            try
+            {
+                var Name = (from obj in _dbContext.Category
+                           where obj.Name == Category.Name  
+                           select obj.Name).FirstOrDefault();   
+
+                if (Name != null)
+                {
+                    TempData["Error"] = "Category Name already Exists !";
+                    return View();
+                }
+
+                _dbContext.Category.Add(Category);
+                _dbContext.SaveChanges();
+
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+                return View();  
+            }           
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            try
+            {
+                if (Id == null || Id == 0)
+                {
+                    TempData["Error"] = "Please check again something is Wrong";
+                    return View();
+                }
+                Category? category = _dbContext.Category.FirstOrDefault(x => x.Id == Id);
+                if (category == null)
+                {
+                    TempData["Error"] = "Please check again something is Wrong";
+                    return View();
+                }
+                return View(category);
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+                return View();  
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            try
+            {
+                var Name = (from oj in _dbContext.Category
+                                  where oj.Name == category.Name
+                                  && oj.Id != category.Id
+                                  select oj.Name).FirstOrDefault();
+
+                if (Name != null)
+                {
+                        TempData["Error"] = "Category Name Already Exists !";
+                        return View();
+                }
+                if (ModelState.IsValid)
+                {
+                    _dbContext.Category.Update(category);
+                    _dbContext.SaveChanges();
+                    TempData["success"] = "Category updated Successfully";
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+                return View();
+            }           
+        }
+
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _dbContext.Category.FirstOrDefault(x => x.Id == Id);
+            if (category == null)
+            {
+                TempData["Error"] = "There is no data to delete";
+                return View();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? Id)
+        {
+            Category? category = _dbContext.Category.Find(Id);
+            _dbContext.Category.Remove(category);
             _dbContext.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
             return RedirectToAction("Index");
         }
     }
--- Fortune/Models/Category.cs
+++ Fortune/Models/Category.cs
@@ -9,8 +9,11 @@ namespace Fortune.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(10,ErrorMessage = "Name is neccessary")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "The Category Name can only contain letters.")]
         public string Name { get; set; }
         [DisplayName("Display Number")]
+        [Range(1,100,ErrorMessage = "custom validation")]
         public int DisplayNumber { get; set; }
     }
 }
