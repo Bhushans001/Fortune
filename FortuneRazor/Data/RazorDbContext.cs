7e75d7aa4011eaef31a6ec788e2ba96a764bd4d0
DigitechPC <DigitechPC@DESKTOP-P9PA386>
2023-11-05 19:50:45 +0530

    Added new project FortuneRazor

---
+++ FortuneRazor/Data/RazorDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FortuneRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FortuneRazor.Data
+{
+    public class RazorDbContext : DbContext
+    {
+        public RazorDbContext(DbContextOptions<RazorDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Category { get; set; }   
+    }
+}
