ffc7aed7c9d19cdbdc58993e9be9a4f4a78218a5
bhushan <bhushan@crytonixtech.com>
2023-12-11 12:30:29 +0530

    install and updated the packages

--- Fortunes.DataAccess/Data/ApplicationDbContext.cs
+++ Fortunes.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Fortunes.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fortunes.DataAccess
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
